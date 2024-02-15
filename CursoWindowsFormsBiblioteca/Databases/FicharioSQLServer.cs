0a5621fed5f6e2814bba6584e6cfc5f2d4973d21
EllyCruzz <serena_ellen@hotmail.com>
2024-02-15 10:28:04 -0300

    Add files

---
+++ CursoWindowsFormsBiblioteca/Databases/FicharioSQLServer.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoWindowsFormsBiblioteca.Databases
+{
+    internal class FicharioSQLServer
+    {
+        public string Tabela;
+        public bool Status;
+        public string Mensagem;
+        public SQLServerClass Db;
+
+        //construtor
+        public FicharioSQLServer(string tabela)
+        {
+            Status = true;
+            try
+            {
+                Db = new SQLServerClass();
+                Tabela = tabela;
+                Mensagem = "Conexão com a tabela criada com sucesso";
+            }
+            catch (Exception ex)
+            {
+                Status = false;
+                Mensagem = $"Conexão com a tabela com erro: {ex.Message}";
+            }
+        }
+
+        public void Incluir(string id, string jsonUnit)
+        {
+            Status = true;
+            try
+            {
+                //INSERT INTO CLIENTE(ID, JSON) VALUES ('000001', '{...}')
+                var sql = $"INSERT INTO {Tabela} (Id, Json) VALUES ('{id}', '{jsonUnit}')";
+                Db.SQLCommand(sql);
+                Status = true;
+                Mensagem = "Inclusão efetuada com sucesso. Identificador: " + id;
+
+            }
+            catch (Exception ex)
+            {
+                Status = false;
+                Mensagem = "Conexão com o Fichario com erro: " + ex.Message;
+            }
+
+        }
+
+        public string Buscar(string id)
+        {
+            Status = true;
+            try
+            {
+                //SELECT ID, JSON FROM CLIENTE WHERE ID='000001'
+                var sql = $"SELECT Id, Json FROM {Tabela} WHERE Id='{id}'";
+                var dt = Db.SQLQuery(sql);
+                if (dt.Rows.Count > 0)
+                {
+                    string conteudo = dt.Rows[0]["Json"].ToString();
+                    Status = true;
+                    Mensagem = "Inclusão efetuada com sucesso. Identificador: " + id;
+                    return conteudo;
+                }
+                else
+                {
+                    Status = false;
+                    Mensagem = $"Código de cliente {id} não existe";
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Status = false;
+                Mensagem = "Erro ao buscar o conteúdo do identificador: " + ex.Message;
+            }
+            return "";
+        }
+
+        public List<string> BuscarTodos()
+        {
+            Status = true;
+            List<string> List = new List<string>();
+            try
+            {
+                //SELECT ID, JSON FROM CLIENTE
+                var sql = $"SELECT Id, Json FROM {Tabela}";
+                var dt = Db.SQLQuery(sql);
+
+                if (dt.Rows.Count > 0)
+                {
+                    for (int i = 0; i <= dt.Rows.Count - 1; i++)
+                    {
+                        string conteudo = dt.Rows[i]["Json"].ToString();
+                        List.Add(conteudo);
+                    }
+                    return List;
+                }
+                else
+                {
+                    Status = false;
+                    Mensagem = $"Não existem clientes na base de dados";
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Status = false;
+                Mensagem = "Erro ao buscar o conteúdo do identificador: " + ex.Message;
+            }
+            return List;
+        }
+
+        public void Apagar(string id)
+        {
+            Status = true;
+            try
+            {
+                var sql = $"SELECT Id, Json FROM {Tabela} WHERE Id='{id}'";
+                var dt = Db.SQLQuery(sql);
+                if (dt.Rows.Count > 0)
+                {
+                    //DELETE FROM CLIENTE WHERE ID = '00010'
+                    sql = $"DELETE FROM {Tabela} WHERE Id='{id}'";
+                    Db.SQLCommand(sql);
+                    Status = true;
+                    Mensagem = "Deleção efetuada com sucesso. Identificador deletado: " + id;
+                }
+                else
+                {
+                    Status = false;
+                    Mensagem = $"Código de cliente {id} não existe";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Status = false;
+                Mensagem = "Erro ao buscar o conteúdo do identificador: " + ex.Message;
+            }
+        }
+
+        public void Alterar(string id, string jsonUnit)
+        {
+            Status = true;
+            try
+            {
+
+                var sql = $"SELECT Id, Json FROM {Tabela} WHERE Id='{id}'";
+                var dt = Db.SQLQuery(sql);
+                if (dt.Rows.Count > 0)
+                {
+                    //UPDATE CLIENTE SET JSON = '{...}' WHERE ID = '00010'
+                    sql = $"UPDATE {Tabela} SET JSON='{jsonUnit}' WHERE Id='{id}'";
+                    Db.SQLCommand(sql);
+                    Status = true;
+                    Mensagem = "Alteração efetuada com sucesso. Identificador: " + id;
+                }
+                else
+                {
+                    Status = false;
+                    Mensagem = "Alteração não permitida porque o identificador não existe: " + id;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Status = false;
+                Mensagem = "Conexão com o Fichario com erro: " + ex.Message;
+            }
+
+        }
+
+    }
+}
+
---
+++ CursoWindowsFormsBiblioteca/Databases/SQLServerClass.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace CursoWindowsFormsBiblioteca.Databases
+{
+    public class SQLServerClass
+    {
+        public string StringConn;
+        public SqlConnection ConnDB;
+
+        public SQLServerClass()
+        {
+            try
+            {
+                //faz a string de conexao
+                //StringConn = "Data Source=DESKTOP-5EMSTEL;Initial Catalog=ByteBank;User ID=sa;Password=APL0108;Encrypt=False";
+                StringConn = ConfigurationManager.ConnectionStrings["Fichario"].ConnectionString;
+                //faz a conexão
+                ConnDB = new SqlConnection(StringConn);
+                //abre a conexão
+                ConnDB.Open();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        //retorna dados, uso isso pra fazer uma consulta ao banco de dados SELECT
+        public DataTable SQLQuery(string sql)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                //prepara um pacote(comando + conexao) pra ser enviado pro banco
+                var myCommand = new SqlCommand(sql, ConnDB);
+                //define o timeout como 0 ou seja todo tempo necessário pra executar
+                myCommand.CommandTimeout = 0;
+                //lê o comando
+                var reader = myCommand.ExecuteReader();
+                //pega o resultado que deu no reader e transforma em Tabela de memória(DataTable)
+                dt.Load(reader);
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return dt;
+        }
+        //caso eu queira executar um comando do banco de dados UPDATE, DELETE ou INSERT
+        public string SQLCommand(string sql)
+        {
+            try
+            {
+                //prepara um pacote(comando + conexao) pra ser enviado pro banco
+                var myCommand = new SqlCommand(sql, ConnDB);
+                //define o timeout como 0 ou seja todo tempo necessário pra executar
+                myCommand.CommandTimeout = 0;
+                //lê o comando
+                var reader = myCommand.ExecuteReader();
+                return "";
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        //fechar a conexão
+        public void CloseConn()
+        {
+            ConnDB.Close();
+        }
+    }
+}
